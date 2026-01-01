766ea6e9edab4b86b25ecba47abd65a0a25d4ca9
shadiyashirin <vshadiyashirin2003@gmail.com>
2026-01-01 20:55:37 +0530

    'faculty-model-edited'

--- etlab_demo/etlab_demo/settings.py
+++ etlab_demo/etlab_demo/settings.py
@@ -17,6 +17,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Application definition
 
@@ -27,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core',
 ]
 
