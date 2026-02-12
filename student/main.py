7346adaa67f1068a12ecc4b92ceedabee00a38fe
Jiya K C <jiyachandriyan@gmail.com>
2026-02-12 13:59:27 +0530

    Added dashboard cards and results page

--- student/main.py
+++ student/main.py
@@ -1,8 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication
 from login_modern_test import LoginPage
 from student_dashboard import StudentDashboard
-
 
 
 class AppController:
@@ -10,21 +9,15 @@ class AppController:
         self.login = LoginPage(self.open_dashboard)
         self.login.show()
 
-    def open_dashboard(self, student_name):
-        self.dashboard = StudentDashboard(student_name)
-        self.dashboard.logout_requested.connect(self.show_login)
+    def open_dashboard(self, username):
+        self.dashboard = StudentDashboard(username)
         self.dashboard.show()
-
-    def handle_logout(self):
-        self.dashboard=None
-        self.show_login()
-
-    def show_login(self):
-        self.login = LoginPage(self.open_dashboard)
-        self.login.show()
+        self.login.close()   # move close here
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    controller = AppController()
-    sys.exit(app.exec_())
+
+    controller = AppController()   # keep reference
+
+    sys.exit(app.exec())
